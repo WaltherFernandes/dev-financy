0d75580f8a343876c4d16900e6d10d97dbd125cb
Walther Augusto Aparecido Fernandes <waltheraaf@gmail.com>
2021-02-13 03:57:28 -0300

    end add transaction

---
+++ README.md
@@ -0,0 +1,32 @@
+# dev.finance$
+A practical project applying knowledge acquired in the Rocketseat's Discover Course
+
+
+<p align="center">
+<img src="https://i.imgur.com/Hlbdl5Z.png" />
+</p>
+
+## Differences from the original project
+
+* Authoral project using SASS
+* Transaction edit option
+* Automatically money format based on the navigator language (I'm starter)
+* Custom error message
+* Some different ways to do the JavaScript code
+
+## Rockeseat and FCamara who am I?
+🚏 My name is Walther, I'm 16, and I'm from Barão de Cocais, MG, Brazil but at 2019 I moved to Timóteo, MG, Brazil to join the institution CEFET-MG, where I fell in love with programming.    
+📚 I'm currently studying systems development technical course, 11th grade. Until then already learned data structure, programming logic initially with Portugol Studio, HTML, CSS Basic, JS Basic, Java, PHP, JQuery and Bootstrap at the course.
+👨‍💻 The RocketSeat's Discorver course (Big Mayk ❣) boosted my knowledge in an incredible way in addition to the enchanting experience I had at the last NLW. I believe that the course would help to further level up, having a course set up exclusively for that.
+
+<p align="center">
+    <a align="center" href="https://smallpdf.com/shared#st=38abedfc-c54d-46ba-9652-51506ce833fe&fn=declaracao_20192009906.pdf&ct=1613187977193&tl=share-document&rf=link">Link with the aforementioned institution</a>
+</P>
+
+## Technologies
+* [ ] HTML
+* [ ] CSS
+* [ ] JS
+
+## Base project author
+Rocketseat
--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,239 @@ const Modal = {
         document.querySelector('div.modal-overlay').classList.remove('active');
     }
 }
+
+const transactions = [
+    {
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021'
+    },
+    {
+        description: 'Criação de Website',
+        amount: 500000,
+        date: '23/01/2021'
+    },
+    {
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021'
+    },
+    {
+        description: 'App',
+        amount: 200000,
+        date: '23/01/2021'
+    }
+];
+
+const Transaction = {
+    all: transactions,
+    add(transaction) {
+        this.all.push(transaction);
+        App.reload();
+    }, 
+    remove(index) {
+        if(index >= this.all.length) reload;
+        this.all.splice(index, 1);
+
+        App.reload();
+    },
+    incomes() {
+        // Sum incomes
+        let income = 0;
+        this.all.forEach((item) => {
+            if(item.amount > 0){
+                income += item.amount;
+            }
+        })
+
+        return income;
+    },
+    expenses() {
+        // Sum expenses
+        let expense = 0;
+        this.all.forEach((item) => {
+            if(item.amount < 0){
+                expense += item.amount;
+            }
+        })
+
+        return expense*=-1;
+    },
+    total() {
+        // Calculate the final amount (incomes - expenses)
+        return this.incomes() - this.expenses();
+    }
+}
+
+let regionCode = 'BRL';
+
+
+const Utils = {
+    formatCurrency(amount) {
+        amount = Number(amount);
+        const signal = amount < 0 ? '-' : '';
+        amount = String(amount/100).replace('-', ' ');
+
+        const options = {style: 'currency', currency: regionCode}; // I created a personalized option for better maintenance code read
+        amount = Number(amount).toLocaleString(navigator.language, options); // I used navigator language to format
+        return `${signal} ${amount}`;
+    },
+    formatAmount(amount) {
+        amount = Number(String(amount).replaceAll(',', '.')) * 100;
+        return Number(amount.toFixed(2));
+    },
+    formatDate(date) {
+        const splittedDate = date.split('-');
+        splittedDate.reverse();
+        return splittedDate.join('/');
+    }
+}
+
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+    getValues(){
+        return {
+            description: this.description.value, 
+            amount: this.amount.value,
+            date: this.date.value
+        }
+    },
+    validateFields(){
+        const { description, amount, date } = this.getValues();
+        if(description.trim() === '' || amount.trim() === '' || date.trim() === ''){
+            throw new Error('Por favor, preencha todos os campos!');
+        }
+        return true;
+     },
+    formatValues() {
+        let { description, amount, date } = this.getValues();
+
+        amount = Utils.formatAmount(amount);
+        date = Utils.formatDate(date);
+
+        return {description, amount, date};
+    },
+    clearFields(){
+        description.value = '';
+        amount.value = '';
+        date.value = '';
+    },
+    submit(event){
+        event.preventDefault();
+
+        try {
+            this.validateFields();
+
+            const transaction = this.formatValues();
+            Transaction.add(transaction);
+
+            this.clearFields();
+
+            Modal.close();
+        } catch (error) {
+            App.alert(error.message)
+        }
+
+    }
+}
+
+const App = {
+    init(){
+        DOM.updateBalance();
+        Transaction.all.forEach((item) => {
+            DOM.addTransaction(item);
+        })
+    },
+    reload(){
+        DOM.clearTransactions();
+        this.init();
+    },
+    alert(errmsg){
+        DOM.alertContainer.classList.remove('invisible');
+        DOM.alertContainer.firstElementChild.innerText = errmsg;
+        setTimeout(() => {
+            DOM.alertContainer.classList.add('invisible');
+            DOM.alertContainer.firstElementChild.innerText = '';
+        }, 3000);
+    },
+    configs() {// Get automatically
+        const region = navigator.language.slice(-2).toLowerCase();
+        (async () => {
+            link = await fetch(`https://restcountries.eu/rest/v2/alpha/${region}`)
+                .then(data => data.json()).then(r => r )
+            regionCode = link.currencies[0].code;
+        
+            // Reload informations with the current local money
+            App.init();
+        })();
+        // Close modal when click modal's background
+        document.querySelector('div.modal-overlay').onclick = function(e) {
+            if(e.target === this ){
+                Modal.close();
+            }
+        }
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('table#data-table tbody'),
+    alertContainer: document.querySelector('div.alert'),
+    clearTransactions(){
+        this.transactionsContainer.innerHTML = '';
+    },
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = this.innerHTMLTransaction(transaction);
+
+        this.transactionsContainer.appendChild(tr); // I used 'THIS'
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSClass = transaction.amount >= 0 ? 'income' : 'expense';
+        const amount = Utils.formatCurrency(transaction.amount);
+
+        const html = `
+                        <td class="description">${transaction.description}</td>
+                        <td class="${CSSClass}">${amount}</td>
+                        <td class="date">${transaction.date}</td>
+                        <td><img src="./assets/img/minus.svg" alt="Remover transação"></td>
+                    `;
+        return html;
+    },
+    updateBalance(){
+        document.querySelector('div p#income-display').innerHTML = Utils.formatCurrency(Transaction.incomes());
+        document.querySelector('div p#expense-display').innerHTML = Utils.formatCurrency(Transaction.expenses());
+        document.querySelector('div p#total-display').innerHTML = Utils.formatCurrency(Transaction.total());
+    }
+}
+
+window.onload = () => App.configs(); 
+
+
+
+
+
+
+
+
+// const tbody = document.querySelector('table#data-table tbody'); AQUI EU COMECEI UM PROTÓTIPO ANTES DA AULA DO MAYK
+// tbody.innerHTML = '';
+// transactions.forEach((item) => {
+//     let tr = document.createElement('tr');
+
+//     let id = document.createElement('td');
+//     let description = document.createElement('td');
+//     let amount = document.createElement('td');
+//     let date = document.createElement('td');
+
+//     description.innerText = item.description;
+//     amount.innerText = item.amount;
+//     date.innerText = item.date;
+
+//     tr.appendChild(description);
+//     tr.appendChild(amount);
+//     tr.appendChild(date);
+
+//     tbody.appendChild(tr);
+// })
